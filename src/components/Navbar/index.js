7acda40de4a1cf273ee4ea2506c6ca1e8b9c4bb6
Tutku Eroğlu <rrandqua@gmail.com>
2023-01-24 01:07:08 +0300

    edited navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./index.css"
+import Home from "../../Pages/Home"
 
 const Navbar = () => {
+  const [vis, setVis] = useState(false)
+  const [activePage, setActivePage] = useState()
+
   return (
+    <>
     <div className='nav'>
       <div className='nav-name'>TUTKU EROĞLU</div>
       <div className='nav-menu'>
-        <span className='menu-tags home'>Anasayfa</span>
+        <span className="menu-tags home">Anasayfa</span>
         <span className='menu-tags'>Hakkımda</span>
         <span className='menu-tags'>Becerilerim</span>
         <span className='menu-tags'>Projelerim</span>
         <span className='menu-tags'>İletişim</span>
       </div>
     </div>
+    </>
   )
 }
 
