b6783862fa831fe7adc037079a4d9a145694fb15
Tutku Eroğlu <rrandqua@gmail.com>
2023-01-24 01:07:24 +0300

    edited app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Navbar from "./components/Navbar"
+import Navbar from "./Components/Navbar"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
 
 function App() {
+  
   return (
-    <>
+    <div className='App'>
       <Navbar />
-    </>
+      <Home />
+      <About />
+    </div>
   );
 }
 
